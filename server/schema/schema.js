2cb685dba38ac8d07316c1846fe51f489c810f4b
CBBell99 <chrisbeckertonbell@gmail.com>
2023-02-15 11:19:19 -0800

    add clientid query

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -23,11 +23,16 @@ const ClientType = new GraphQLObjectType({
 const ProjectType = new GraphQLObjectType({
   name: 'Project',
   fields: () => ({
-    id: {type:  GraphQLID},
-    // clientId: {type: GraphQLID},
-    name: {type: GraphQLString},
-    description: {type: GraphQLString},
-    status: {type: GraphQLString}
+    id: { type: GraphQLID },
+    name: { type: GraphQLString },
+    description: { type: GraphQLString },
+    status: { type: GraphQLString },
+    client: {
+      type: ClientType,
+      resolve(parent, args) {
+        return clients.find((client) => client.id === parent.clientId);
+      },
+    },
   }),
 });
 
