b095cb54840eb9e046dc113c87101594a92dcc49
CBBell99 <chrisbeckertonbell@gmail.com>
2023-02-23 14:10:02 -0800

    use cache method instead

--- client/src/components/ClientRow.svelte
+++ client/src/components/ClientRow.svelte
@@ -8,7 +8,16 @@
 
 	const deleteClient = mutation(DELETE_CLIENT, {
 		variables: { id: client.id },
-		refetchQueries: [{ query: GET_CLIENTS }],
+		// refetchQueries: [{ query: GET_CLIENTS }],
+    update(cache, {data: {deleteClient}}) {
+      const {clients} = cache.readQuery({
+        query: GET_CLIENTS
+      })
+        cache.writeQuery({
+          query: GET_CLIENTS,
+          data: {clients: clients.filter(client => client.id !== deleteClient.id)}
+      })
+    }
 	});
 </script>
 
