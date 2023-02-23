dbfbf9612bc3ad1a2b50ee618b1d2642e8f9f0ec
CBBell99 <chrisbeckertonbell@gmail.com>
2023-02-23 10:34:39 -0800

    move query into own component

--- client/src/components/ClientRow.svelte
+++ client/src/components/ClientRow.svelte
@@ -1,9 +1,16 @@
 <script>
   export let client
+  import Fa from "svelte-fa"
+  import {faTrash} from "@fortawesome/free-solid-svg-icons"
 </script>
 
 <tr>
 	<td>{client.name}</td>
 	<td>{client.email}</td>
 	<td>{client.phone}</td>
+  <td>
+    <button class="btn btn-danger btn-sm">
+      <Fa icon={faTrash}/>
+     </button>
+  </td>
 </tr>
--- client/src/components/Clients.svelte
+++ client/src/components/Clients.svelte
@@ -1,44 +1,34 @@
 <script>
-	import { gql } from '@apollo/client/core';
 	import { query } from 'svelte-apollo';
 	import ClientRow from './ClientRow.svelte';
-	// import {client} from "../routes/+page.svelte"
+  import Spinner from './Spinner.svelte';
+  import { GET_CLIENTS } from '../queries/clientQueries';
 
-	const GET_CLIENTS = gql`
-		query getClients {
-			clients {
-				id
-				name
-				email
-				phone
-			}
-		}
-	`;
 	const clients = query(GET_CLIENTS);
 </script>
 
 <div>Clients</div>
 <div>
-{#if $clients.loading}
-  <p>Loading...</p>
-{:else if $clients.error}
-  <p>Error: Something Went Wrong</p>
-{:else}
-  {#each $clients.data.clients as client}
-  <table class="table table-hover mt-3">
-    <thead>
-      <tr>
-        <th>Name</th>
-        <th>Email</th>
-        <th>Phone</th>
-        <th></th>
-      </tr>
-    </thead>
-    <tbody>
-      <ClientRow client={client}/>
-    </tbody>
-  </table>
-  <br/>
-  {/each}
-{/if}
+	{#if $clients.loading}
+		<Spinner/>
+	{:else if $clients.error}
+		<p>Error: Something Went Wrong</p>
+	{:else}
+		{#each $clients.data.clients as client}
+			<table class="table table-hover mt-3">
+				<thead>
+					<tr>
+						<th>Name</th>
+						<th>Email</th>
+						<th>Phone</th>
+						<th />
+					</tr>
+				</thead>
+				<tbody>
+					<ClientRow {client} />
+				</tbody>
+			</table>
+			<br />
+		{/each}
+	{/if}
 </div>
