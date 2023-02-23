569e9939c45d52d05c8da9d9c4a483d75752301f
CBBell99 <chrisbeckertonbell@gmail.com>
2023-02-23 14:09:51 -0800

    delete from db

--- client/src/components/ClientRow.svelte
+++ client/src/components/ClientRow.svelte
@@ -1,16 +1,26 @@
 <script>
-  export let client
-  import Fa from "svelte-fa"
-  import {faTrash} from "@fortawesome/free-solid-svg-icons"
+	export let client;
+	import Fa from 'svelte-fa';
+	import { faTrash } from '@fortawesome/free-solid-svg-icons';
+	import { DELETE_CLIENT } from '../mutations/clientMutations';
+	import { mutation } from 'svelte-apollo';
+	import { GET_CLIENTS } from '../queries/clientQueries';
+
+	const deleteClient = mutation(DELETE_CLIENT, {
+		variables: { id: client.id },
+		refetchQueries: [{ query: GET_CLIENTS }],
+	});
 </script>
 
-<tr>
-	<td>{client.name}</td>
-	<td>{client.email}</td>
-	<td>{client.phone}</td>
-  <td>
-    <button class="btn btn-danger btn-sm">
-      <Fa icon={faTrash}/>
-     </button>
-  </td>
-</tr>
+<tbody>
+	<tr>
+		<td>{client.name}</td>
+		<td>{client.email}</td>
+		<td>{client.phone}</td>
+		<td>
+			<button class="btn btn-danger btn-sm" on:click={deleteClient}>
+				<Fa icon={faTrash} />
+			</button>
+		</td>
+	</tr>
+</tbody>
--- client/src/components/Clients.svelte
+++ client/src/components/Clients.svelte
@@ -1,8 +1,8 @@
 <script>
 	import { query } from 'svelte-apollo';
 	import ClientRow from './ClientRow.svelte';
-  import Spinner from './Spinner.svelte';
-  import { GET_CLIENTS } from '../queries/clientQueries';
+	import Spinner from './Spinner.svelte';
+	import { GET_CLIENTS } from '../queries/clientQueries';
 
 	const clients = query(GET_CLIENTS);
 </script>
@@ -10,25 +10,22 @@
 <div>Clients</div>
 <div>
 	{#if $clients.loading}
-		<Spinner/>
+		<Spinner />
 	{:else if $clients.error}
 		<p>Error: Something Went Wrong</p>
 	{:else}
-		{#each $clients.data.clients as client}
-			<table class="table table-hover mt-3">
-				<thead>
-					<tr>
-						<th>Name</th>
-						<th>Email</th>
-						<th>Phone</th>
-						<th />
-					</tr>
-				</thead>
-				<tbody>
-					<ClientRow {client} />
-				</tbody>
-			</table>
-			<br />
-		{/each}
+		<table class="table table-hover mt-3">
+			<thead>
+				<tr>
+					<th>Name</th>
+					<th>Email</th>
+					<th>Phone</th>
+					<th />
+				</tr>
+			</thead>
+			{#each $clients.data.clients as client}
+				<ClientRow {client} />
+			{/each}
+		</table>
 	{/if}
 </div>
